c789c7b875f9ffeec7487fd21e8b43e59ea3ec27
zmy <1784311404@qq.com>
2019-03-20 10:31:00 +0800

    小程序简单组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,292 @@ Page({
    * 页面的初始数据
    */
   data: {
+    height: 20,
+    focus: false,
+      text: "This is Page data.",
+      texts: "hello 小程序",
+      view: 'APP',
+      imgUrls: [
+        '/static/images/3.jpg',
+        '/static/images/4.jpg',
+        '/static/images/1.bmp'
+      ],
+      // 小图标
+      iconType: [
+        'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+      ],
+      //  多项选择器
+      items: [
+        { name: 'USA', value: '美国' },
+        { name: 'CHN', value: '中国', checked: 'true' },
+        { name: 'BRA', value: '巴西' },
+        { name: 'JPN', value: '日本' },
+        { name: 'ENG', value: '英国' },
+        { name: 'TUR', value: '法国' },
+      ],
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+    objectMultiArray: [
+      [
+        {
+          id: 0,
+          name: '无脊柱动物'
+        },
+        {
+          id: 1,
+          name: '脊柱动物'
+        }
+      ], [
+        {
+          id: 0,
+          name: '扁性动物'
+        },
+        {
+          id: 1,
+          name: '线形动物'
+        },
+        {
+          id: 2,
+          name: '环节动物'
+        },
+        {
+          id: 3,
+          name: '软体动物'
+        },
+        {
+          id: 3,
+          name: '节肢动物'
+        }
+      ], [
+        {
+          id: 0,
+          name: '猪肉绦虫'
+        },
+        {
+          id: 1,
+          name: '吸血虫'
+        }
+      ]
+    ],
+    multiIndex: [0, 0, 0],
+    date: '2016-09-01',
+    time: '12:01',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
+    markers: [{
+      iconPath: '/resources/others.png',
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: '#FF0000DD',
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  bindViewTap: function (event) {
+    this.setData({
+      text: "This is Page data.",
+      texts: "你点击了一个按钮"
+    });
+    
+  },
+  
+  bindViewPress: function(event) {
+    this.setData({
+      texts: "hello 小程序",
+      text: "你长按了一个按钮"
+      //  console.log(event)
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  progressETL: function (event) {
+    this.setData({
+      success: "加载完成"
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset() {
+    console.log('form发生了reset事件')
+  },
+  // input 输入框
+  dones: function (event) {
+    this.setData({
+      aa: "输入完成"
+    });
+  },
+  
+  // 选择器
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindMultiPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange(e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value)
+    const data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    }
+    data.multiIndex[e.detail.column] = e.detail.value
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物']
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫']
+            break
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物']
+            data.multiArray[2] = ['鲫鱼', '带鱼']
+            break
+        }
+        data.multiIndex[1] = 0
+        data.multiIndex[2] = 0
+        break
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫']
+                break
+              case 1:
+                data.multiArray[2] = ['蛔虫']
+                break
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥']
+                break
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓']
+                break
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物']
+                break
+            }
+            break
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼']
+                break
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼']
+                break
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎']
+                break
+            }
+            break
+        }
+        data.multiIndex[2] = 0
+        break
+    }
+    console.log(data.multiIndex)
+    this.setData(data)
+  },
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  // textarea
+  bindButtonTap() {
+    this.setData({
+      focus: true
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  bindTextAreaBlur(e) {
+    console.log(e.detail.value)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  bindFormSubmit(e) {
+    console.log(e.detail.value.textarea)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad(options) {
+    this.setData({
+      title: options.title
+    })
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
   }
+ 
+
+
 })
